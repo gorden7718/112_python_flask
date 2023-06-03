4fbbe17695efbfe4ea29cac01c112bf116944ab9
gorden7718 <gorden7718@gmail.com>
2023-06-03 11:00:58 +0800

    2

--- lesson9_1.py
+++ lesson9_1.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
 st.write("## Hello world")
-st.write("## **My name is Gorden**")
+"## **My name is Gorden**"
+st.divider()
+st.markdown("Hello world")
+st.divider()
+st.title("This is title")
+st.divider()
+st.header("This is header")
+st.divider()
+st.subheader("This is subheader")
+st.divider()
+st.caption("This is caption")
+st.divider()
+st.code("if not ture")
