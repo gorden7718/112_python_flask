70caeced1c988de8f1a8b9855ca0ac8db5cf0436
gorden7718 <gorden7718@gmail.com>
2023-06-10 15:47:58 +0800

    4

--- lesson12_1.py
+++ lesson12_1.py
@@ -11,3 +11,17 @@ with st.sidebar:
 for code in selected_codes:
     code = code[:4]+'.TW'
     st.write(code)
+
+
+@st.cache_data
+def fetch_stock_dataFrame(id):
+    stock_dataFrame = yf.download(id,start='2022-01-01')
+    return stock_dataFrame
+
+for code in selected_codes:
+    code1 = code[:4]+'.TW'
+    code_stock_dataFrame = fetch_stock_dataFrame(code1)
+    code_stock_dataFrame_sorted = code_stock_dataFrame.sort_index(ascending=False)
+    st.subheader(code)
+    st.dataframe(code_stock_dataFrame_sorted,width=1024)
+    st.divider()
